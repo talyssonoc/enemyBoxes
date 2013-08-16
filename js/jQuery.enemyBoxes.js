089f7ccfec1d721812cf7e9918b81884f70e336b
Talysson de Oliveira Cassiano <talyssonoc@gmail.com>
2013-08-16 08:42:03 -0300

    Changed the listener to listen to "change" instead of "click".

--- js/jQuery.enemyBoxes.js
+++ js/jQuery.enemyBoxes.js
@@ -1,14 +1,15 @@
 (function($) {
 	$.fn.enemyBoxes = function() {
-		var selector = this.selector;
+		var selector = this.selector; //Get the selector used
 
 		return this.each(function() {
 
-			$(this).on("click", function() {
+			$(this).on('change', function() { //Creates a change listener on every element that matches the selector
 				var element = this;
 
-				if($(this).is(":checked")) {
-					$(selector).not(element).attr("checked", false);
+				//If a matching element changes, then it unchecks all its enemy boxes
+				if($(this).is(':checked')) {
+					$(selector).not(element).attr('checked', false);
 				}
 
 			});
